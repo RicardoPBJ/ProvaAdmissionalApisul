053a458f9d6ec6c8cee237daedaa7729475e5bed
Ricardo Pereira <rbrumjunio@gmail.com>
2025-06-18 19:42:24 -0300

    Iniciando a prova admissional da APISul

---
+++ C#/ElevadorService.cs
@@ -0,0 +1,75 @@
+using System;
+
+
+namespace ProvaAdmissionalCSharpApisul
+{
+  public class ElevadorService : IElevadorService
+  {
+    public List<int> andarMenosUtilizado()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public List<char> elevadorMaisFrequentado()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public List<char> periodoMaiorFluxoElevadorMaisFrequentado()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public List<char> elevadorMenosFrequentado()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public List<char> periodoMenorFluxoElevadorMenosFrequentado()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public List<char> periodoMaiorUtilizacaoConjuntoElevadores()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public float percentualDeUsoElevadorA()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public float percentualDeUsoElevadorB()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public float percentualDeUsoElevadorC()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public float percentualDeUsoElevadorD()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+
+    public float percentualDeUsoElevadorE()
+    {
+      // Implementação do método
+      throw new NotImplementedException();
+    }
+  }
+
+}
